9d2e2d7a2ef20286af36fae32f6ea3a50aec84c2
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-10-21 01:00:15 +0300

    two components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  onLeaveFeedback = option => {
+    this.setState(prevState => (prevState[option] += 1));
+  };
+  render() {
+    return (
+      <>
+        {/* <Section title=""></Section> */}
+
+        <h1>Please leave feedback</h1>
+        <div>
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          ></FeedbackOptions>
+
+          {/* <Section title=""></Section> */}
+          <Statistics
+            onLeaveFeedback={this.onLeaveFeedback}
+            good={this.good}
+            neutral={this.neutral}
+            bad={this.bad}
+            total={this.total}
+            positivePercentage={this.positivePercentage}
+          ></Statistics>
+        </div>
+      </>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div>
+    {options.map(option => (
+      <button type="button" onClick={() => onLeaveFeedback(option)}>
+        {option}
+      </button>
+    ))}
+  </div>
+);
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+export default FeedbackOptions;
