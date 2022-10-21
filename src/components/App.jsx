7dd7f51ffa641d46d6d5d08ac8d3dbe6dbc1b22b
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-10-22 01:44:09 +0300

    ee

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 import Section from './Section/Section';
